d84561288ffe5edbcecda5525e34e4a977812532
srideviaishwariya@gmail.com <sridevi@adobe.com>
2013-11-15 15:59:20 -0800

    added testcases for edit text

--- calabash-android/features/start_app.feature
+++ calabash-android/features/start_app.feature
@@ -7,4 +7,13 @@ Feature: Verify if application opens
     Given I am on the Main Screen
   	When I press Test Me button
   	Then I should see Welcome to Calabash world
-  	
+
+  Scenario: Enter Text in Edit Text
+    Given I am on the Main Screen
+    When I type Hello World in Text Editor
+    Then I should see Hello World in Text Editor
+
+  Scenario: Verify textview is correct
+    Given I am on the Main Screen
+    Then I should see "Hello Calabash!" text
+
--- calabash-android/features/step_definitions/verify_userinterface_steps.rb
+++ calabash-android/features/step_definitions/verify_userinterface_steps.rb
@@ -1,22 +1,41 @@
 require 'calabash-android/calabash_steps'
 
 Given(/^I am on the Main Screen$/) do
-    
+  check_element_exists("textview marked:'Hello Calabash!'")
 end
 
 When(/^I press Test Me button$/) do
-	touch(query("button"))
+  check_element_exists("button marked:'Test Me'")
+  touch(query("button marked:'Test Me'"))
 end
 
 Then(/^I should see Welcome to Calabash world$/) do
-	result = query "textview", :text
-	result.each do |element|
-		if element == 'Welcome to Calabash world' then puts element
-        end
-    end
+  result = query("textview id:'btnLabel'", 'text').first
+  if result != 'Welcome to Calabash world'
+    fail(msg="The buttonClick did not work as expected")
+  end
+end
+
+When(/^I type Hello World in Text Editor$/) do
+  query('edittext', :setText => 'Hello World')
+end
+
+Then(/^I should see Hello World in Text Editor$/) do
+  check_element_exists("edittext marked:'Hello World'")
 end
 
 Then(/^do nothing for now$/) do
-    
+
 end
+
+Then(/^I should see "(.*?)" text$/) do |txtName|
+  result = query('textview', :text)
+  result.each do |element|
+    if element == txtName then puts element
+    end
+  end
+end
+
+
+
 
